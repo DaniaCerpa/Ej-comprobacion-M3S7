b3306bcaa29373d626807e1b591cd624e20c987d
DaniaCerpa <dania.cerpa@gmail.com>
2024-08-23 00:51:24 -0400

    Primer commit

---
+++ controlflujo.py
@@ -0,0 +1,10 @@
+lista_numeros = [4, 7, 8, 2, 0, 6, 9, -1, 15,88]
+
+for numero in lista_numeros:
+    if numero == 0:
+        print (numero, "es cero")
+    elif  numero % 2 == 0:
+        print (numero, "es par")
+    else:
+        print (numero, "es impar")
+      
